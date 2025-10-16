ea8a9df53c1b0c977fdb406c5cf010a51b3a2f3e
wsuchanek <william.suchanek@gmail.com>
2025-10-15 22:18:21 -0400

    Added player object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -9,11 +10,20 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
